5eaebe2de2424bb04a711785321e798ca06a0f3d
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-04-25 10:48:00 +0100

    first commit

--- run_gp.py
+++ run_gp.py
@@ -11,11 +11,11 @@ def compute_file_name(p):
 
 acc_list = []
 run = 1
-rRates = {'cov1':0.,'cov2':0.,'fc1': 4.,'fc2':0.}
-cRates = {'cov1':0.,'cov2':0.,'fc1': 4.,'fc2':0.}
+rRates = {'cov1':0.,'covr':0.,'fc1': 5.,'fc2':0.}
+cRates = {'cov1':0.,'cov2':0.,'fc1': 5.,'fc2':0.}
 f_name = compute_file_name(cRates)
 print(f_name)
-while (cRates['fc1'] <= 5.):
+while (cRates['fc1'] <= 8.):
     iter_cnt = 0
     cRates['fc1'] = cRates['fc1'] + 0.2
     rRates['fc1'] = rRates['fc1'] + 0.2
