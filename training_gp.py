4f2335e4cc198d2a306715be8f89ead216093a49
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-04-26 21:37:17 +0100

    first commit

--- training_gp.py
+++ training_gp.py
@@ -324,7 +324,7 @@ def recover_weights(weights_mask, grad_probs, recover_rates):
         mask.astype(int)
         weights_mask[key] = weights_mask[key] + mask
     mask_info(weights_mask)
-    # sys.exit()
+    sys.exit()
 
     return (weights_mask)
 '''
