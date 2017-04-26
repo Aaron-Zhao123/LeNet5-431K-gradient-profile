ede3601c5672b2366d49b4b134cfec425ba3f75c
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-04-26 22:19:07 +0100

    first commit

--- training_gp.py
+++ training_gp.py
@@ -495,10 +495,15 @@ def main(argv = None):
                 non_zeros,size =calculate_non_zero_weights(1-weights_mask['cov2'])
                 print(non_zeros)
                 print(weights_mask['cov2'].shape)
+
+                print('my grads')
+                non_zeros,size =calculate_non_zero_weights(collect_grads['cov2'])
+                print(non_zeros)
+                print(collect_grads['cov2'].shape)
+                
                 print('my masked grads')
                 non_zeros,size =calculate_non_zero_weights(grad_mask_val['cov2'])
                 print(non_zeros)
-                print(grad_mask_val['cov2'])
                 print(grad_mask_val['cov2'].shape)
 
                 weights_mask = recover_weights(weights_mask, grad_mask_val, recover_rates)
