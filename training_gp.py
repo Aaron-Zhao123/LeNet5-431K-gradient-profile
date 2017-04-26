fb858c5d51bf0dd7180495dc71eaf4e3288c3062
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-04-26 22:43:03 +0100

    first commit

--- training_gp.py
+++ training_gp.py
@@ -62,7 +62,7 @@ def initialize_variables(parent_dir, model_number, weights_mask, profile = False
         wc2 = wc2 * weights_mask['cov2']
         wd1 = wd1 * weights_mask['fc1']
         out = out * weights_mask['fc2']
-        wc1.astype(np.float32)
+        wc1 = wc1.astype(np.float32)
         wc2.astype(np.float32)
         wd1.astype(np.float32)
         out.astype(np.float32)
