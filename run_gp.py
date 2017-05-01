aea6e49147017b09047b711c436b8cf0ba96dd34
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-05-01 11:51:24 +0100

    added key

--- run_gp.py
+++ run_gp.py
@@ -11,15 +11,15 @@ def compute_file_name(p):
 
 acc_list = []
 run = 1
-rRates = {'cov1':0.,'cov2':0.,'fc1': 6.,'fc2':0.}
+rRates = {'cov1':0.,'cov2':3.,'fc1': 4.,'fc2':0.}
 cRates = {'cov1':0.,'cov2':0.,'fc1': 4.7,'fc2':0.}
 f_name = compute_file_name(cRates)
 learning_rate = 1e-4
 print(f_name)
-while (cRates['cov2'] <= 2.5):
+while (cRates['cov2'] <= 2.):
     iter_cnt = 0
     cRates['cov2'] = cRates['cov2'] + .5
-    rRates['cov2'] = rRates['cov2'] + .5
+    # rRates['cov2'] = rRates['cov2'] + .1
     # cRates['fc1'] = cRates['fc1'] + 0.1
     # rRates['fc1'] = rRates['fc1'] + 0.
     while (iter_cnt < 7):
