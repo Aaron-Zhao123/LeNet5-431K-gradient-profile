c225b988ddc2b2f9dc1bd51475e811759173c558
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-05-04 18:38:55 +0100

    retrain

--- run_gp.py
+++ run_gp.py
@@ -12,13 +12,13 @@ def compute_file_name(p):
 acc_list = []
 run = 1
 rRates = {'cov1':0.,'cov2':4.,'fc1': 6.,'fc2':0.}
-cRates = {'cov1':0.,'cov2':0.,'fc1': 4.4,'fc2':0.}
+cRates = {'cov1':0.,'cov2':1.5,'fc1': 4.4,'fc2':0.}
 f_name = compute_file_name(cRates)
 learning_rate = 1e-4
 print(f_name)
 while (cRates['cov2'] <= 4.):
     iter_cnt = 0
-    cRates['cov2'] = cRates['cov2'] + .5
+    cRates['cov2'] = cRates['cov2'] + .1
     # rRates['cov2'] = rRates['cov2'] + 1
     # cRates['fc1'] = cRates['fc1'] + 0.2
     # rRates['fc1'] = rRates['fc1'] + 0.
