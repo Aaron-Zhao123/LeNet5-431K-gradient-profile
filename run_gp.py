c29b228e45ad1e8f9bc480a775e6a3dba4c70038
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-05-08 20:10:12 +0100

    changed profile method

--- run_gp.py
+++ run_gp.py
@@ -13,16 +13,16 @@ acc_list = []
 run = 1
 # rRates = {'cov1':0.,'cov2':4.,'fc1': 6.,'fc2':0.}
 # cRates = {'cov1':0.,'cov2':1.5,'fc1': 4.4,'fc2':0.}
-rRates = {'cov1':0.,'cov2':0.,'fc1': 99.9,'fc2':0.}
-cRates = {'cov1':0.,'cov2':0.,'fc1': 2.,'fc2':0.}
+rRates = {'cov1':0.,'cov2': 95.,'fc1': 99.9,'fc2':0.}
+cRates = {'cov1':0.,'cov2':0.,'fc1': 4.4,'fc2':0.}
 f_name = compute_file_name(cRates)
 learning_rate = 1e-4
 print(f_name)
-while (cRates['fc1'] <= 4.5):
+while (cRates['fc2'] <= 3.):
     iter_cnt = 0
     # cRates['cov2'] = cRates['cov2'] + .1
-    # rRates['cov2'] = rRates['cov2'] + 1
-    cRates['fc1'] = cRates['fc1'] + 0.2
+    rRates['cov2'] = rRates['cov2'] + 1
+    # cRates['fc1'] = cRates['fc1'] + 0.2
     # rRates['fc1'] = rRates['fc1'] + 0.
     while (iter_cnt < 7):
         # Prune
