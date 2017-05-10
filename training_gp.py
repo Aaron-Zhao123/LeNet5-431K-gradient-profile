9e9b72dd0dedd34688bd1bee2cb10df7f8957b8b
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-05-10 11:47:20 +0100

    changed profile method

--- training_gp.py
+++ training_gp.py
@@ -398,7 +398,7 @@ def main(argv = None):
                 r_mask = pickle.load(f)
             weights_mask = {}
             for key in keys:
-                weights_mask[key] = np.logical_or(hard_mask, r_mask).astype(float32)
+                weights_mask[key] = np.logical_or(hard_mask, r_mask, dtype=float32)
         else:
             weights_mask = {
                 'cov1': np.ones([5, 5, NUM_CHANNELS, 20]),
