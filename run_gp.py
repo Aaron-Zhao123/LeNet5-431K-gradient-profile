74f201d2953e7fc02748e5a36f847e68c01e6930
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-05-15 12:49:41 +0100

    retrain

--- run_gp.py
+++ run_gp.py
@@ -14,7 +14,7 @@ run = 1
 # rRates = {'cov1':0.,'cov2':4.,'fc1': 6.,'fc2':0.}
 # cRates = {'cov1':0.,'cov2':1.5,'fc1': 4.4,'fc2':0.}
 rRates = {'cov1':90.,'cov2': 95.,'fc1': 99.8,'fc2':90.}
-cRates = {'cov1':0.6,'cov2':0.,'fc1': 3.5,'fc2': 0.}
+cRates = {'cov1':0.,'cov2':0.8,'fc1': 3.5,'fc2': 0.}
 f_name = compute_file_name(cRates)
 learning_rate = 1e-4
 print(f_name)
